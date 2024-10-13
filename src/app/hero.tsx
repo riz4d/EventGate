41d09a490c37ea64c77224f659859d59bb6dd839
riz4d <rizadvillan@gmail.com>
2024-10-13 11:38:02 -0400

    Refactor hero component imports and remove unused dependencies

--- src/app/hero.tsx
+++ src/app/hero.tsx
@@ -2,44 +2,48 @@
 
 import { Button, Typography } from "@material-tailwind/react";
 import { motion } from 'framer-motion';
+
 function Hero() {
   return (
-    
-      <div className="relative min-h-screen w-full bg-[url('/image/event.jpeg')] bg-cover bg-no-repeat">
+    <div className="relative min-h-screen w-full bg-[url('/image/event.jpeg')] bg-cover bg-no-repeat">
       <div className="absolute inset-0 h-full w-full bg-gray-900/60" />
       <div className="grid min-h-screen px-8">
-      <div className="container relative z-10 my-auto mx-auto grid place-items-center text-center">
-      <motion.div
-          initial={{ opacity: 0, y: -50 }}
-          animate={{ opacity: 1, y: 0 }}
-          transition={{ duration: 0.5 }}
-        >
-        <Typography variant="h1" color="white" className="lg:max-w-3xl">
-          International Designer League 2024
-        </Typography>
-        <Typography
-          variant="lead"
-          color="white"
-          className="mt-1 mb-12 w-full md:max-w-full lg:max-w-2xl"
-        >
-          Join us for the most anticipated event of the year
-        </Typography>
-        </motion.div>
-        <motion.div
-          initial={{ opacity: 0, y: -50 }}
-          animate={{ opacity: 1, y: 0 }}
-          transition={{ duration: 0.5 }}
-        >
-        <div className="flex items-center gap-4">
-          <Button variant="gradient" color="white">
-            Get Ticket
-          </Button>
+        <div className="container relative z-10 my-auto mx-auto grid place-items-center text-center">
+          <motion.div
+            initial={{ opacity: 0, y: -50 }}
+            animate={{ opacity: 1, y: 0 }}
+            transition={{ duration: 0.5 }}
+          >
+            <Typography
+              variant="h1"
+              color="white"
+              className="lg:max-w-3xl"
+              placeholder=""
+              onPointerEnterCapture={() => {}}
+              onPointerLeaveCapture={() => {}}
+            >
+              International Designer League 2024
+            </Typography>
+            <Typography
+              variant="lead"
+              color="white"
+              className="mt-1 mb-12 w-full md:max-w-full lg:max-w-2xl"
+              placeholder=""
+              onPointerEnterCapture={() => {}}
+              onPointerLeaveCapture={() => {}}
+            >
+              Join us for the most anticipated event of the year
+            </Typography>
+          </motion.div>
+          <motion.div
+            initial={{ opacity: 0, y: -50 }}
+            animate={{ opacity: 1, y: 0 }}
+            transition={{ duration: 0.5 }}
+          >
+          </motion.div>
         </div>
-        </motion.div>
       </div>
     </div>
-  </div>
-  
   );
 }
 
